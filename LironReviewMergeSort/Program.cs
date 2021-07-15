83ba2ae0fe3c4b05a3f4527c5331f9d0eed5c7af
Liron Saedi <Liron.Saedi@GMR.local>
2021-07-14 18:16:42 -0700

    I finished

--- LironReviewMergeSort/Program.cs
+++ LironReviewMergeSort/Program.cs
@@ -5,10 +5,7 @@ namespace LironReviewMergeSort
 {
     class Program
     {
-
-
-        // DO THIS WITH ARRAYS LATER
-        public List<T> MergeSort<T>(List<T> list) where T : IComparable<T>
+        static  public List<T> MergeSort<T>(List<T> list) where T : IComparable<T>
         {
             if (list.Count < 2)
             {
@@ -26,20 +23,31 @@ namespace LironReviewMergeSort
             List<T> totalResults = new List<T>();
             int leftCounter = 0;
             int rightCounter = 0;
-            while (leftCounter != leftSide.Count && rightCounter != rightSide.Count)
+            while (leftCounter != leftResult.Count && rightCounter != rightResult.Count)
             {
-                if(leftSide[leftCounter].CompareTo(rightSide[rightCounter]) > 0)
+                if(leftResult[leftCounter].CompareTo(rightResult[rightCounter]) > 0)
                 {
-                    totalResults.Add(rightSide[rightCounter]);
+                    totalResults.Add(rightResult[rightCounter]);
                     rightCounter++;
                 }
-
-                if (leftSide[leftCounter].CompareTo(rightSide[rightCounter]) < 0)
+                else
                 {
-                    totalResults.Add(leftSide[leftCounter]);
+                    totalResults.Add(leftResult[leftCounter]);
                     leftCounter++;
                 }
 
+            }
+
+            while(leftCounter < leftResult.Count)
+            {
+                totalResults.Add(leftResult[leftCounter]);
+                leftCounter++;
+            }
+
+            while (rightCounter < rightResult.Count)
+            {
+                totalResults.Add(rightResult[rightCounter]);
+                rightCounter++;
             }
             return totalResults;
             // merge the resulting lists
@@ -47,6 +55,27 @@ namespace LironReviewMergeSort
         }
         static void Main(string[] args)
         {
+            List<int> list = new List<int>();
+            List<int> sorted = new List<int>();
+            Random gen = new Random(432509823);
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(gen.Next(0, 20));
+            }
+
+            sorted = MergeSort<int>(list);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < sorted.Count; i++)
+            {
+                Console.WriteLine(sorted[i]);
+            }
 
         }
     }
